fa635b1d6f484ba9678b3dc59281943e0e52cb1d
magnolo <office@manfredwalder.at>
2016-10-19 01:45:12 +0200

    extract source data id as variable

--- src/app/modules/map/map.component.ts
+++ src/app/modules/map/map.component.ts
@@ -14,10 +14,11 @@ import { MapService } from './map.service';
 })
 export class MapComponent implements OnInit {
 
+  adminDataSourceID: string = 'admin0';
   admin0LayerID: string = 'admin0shaped';
   admin0Key: string = 'ISO_A2';
 
-  constructor(private mapService: MapService) {}
+  constructor(private mapService: MapService) { }
 
   /**
    * The MapboxGl map will be initialized and the dataSource and layer will be added
@@ -38,15 +39,15 @@ export class MapComponent implements OnInit {
     this.mapService.initMap();
 
     //Add the vector data source for the global admin boundaries
-    this.mapService.addDataSource('admin0', {
+    this.mapService.addDataSource(this.adminDataSourceID, {
       type: 'vector',
-      'url': 'mapbox://magnolo.6zzfq94v'
+      url: 'mapbox://magnolo.6zzfq94v'
     });
 
     //Add the (fill)layer for all country shapes above the water label layer, underneath all country labels
     this.mapService.addLayer({
       id: this.admin0LayerID,
-      source: 'admin0',
+      source: this.adminDataSourceID,
       'source-layer': 'ne_10m_admin_0_countries-5pek43',
       type: 'fill',
       filter: ['!=', this.admin0Key, ''],
@@ -57,9 +58,9 @@ export class MapComponent implements OnInit {
     }, 'water-label');
 
     // Paint the Layer shapes where the iso values fit the colorCodes
-    this.mapService.paintLayer(this.admin0LayerID,this.admin0Key, colorCodes);
+    this.mapService.paintLayer(this.admin0LayerID, this.admin0Key, colorCodes);
 
     // Filter just the shapes which available in colorCodes
-    this.mapService.filterLayer(this.admin0LayerID, this.admin0Key, 'in', colorCodes.map((code) => { return code[0]}));
+    this.mapService.filterLayer(this.admin0LayerID, this.admin0Key, 'in', colorCodes.map((code) => { return code[0] }));
   }
 }
