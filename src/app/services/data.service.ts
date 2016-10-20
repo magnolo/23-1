c605fa83179d79e182f5504820e8880b77732ca6
aldorado <anuar.suarez@hotmail.com>
2016-10-20 11:34:56 +0200

    Mock DataService for Choropleths

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,5 +2,34 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class DataService {
+
+  mockData: any = [
+    {
+      iso: "AT",
+      value: "20"
+    },
+    {
+      iso: "DE",
+      value: "40"
+    },
+    {
+      iso: "ES",
+      value: "60"
+    },
+    {
+      iso: "IT",
+      value: "80"
+    },
+    {
+      iso: "CO",
+      value: "100"
+    },
+  ];
+
   constructor() {  }
+
+  getValuesWithIso() {
+    return this.mockData;
+  }
+
 }
